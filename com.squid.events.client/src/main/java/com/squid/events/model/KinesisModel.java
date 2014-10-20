3548f31a2af78966a000b19a963cbd22fe98a56d
Serge Fantino <serge@squidsolutions.com>
2014-10-20 17:14:24 +0200

    updated model with kinesis model

---
+++ com.squid.events.client/src/main/java/com/squid/events/model/KinesisModel.java
@@ -0,0 +1,21 @@
+package com.squid.events.model;
+
+/**
+ * define some special kinesis field
+ * 
+ * @author sergefantino
+ *
+ */
+public interface KinesisModel {
+    
+    /**
+     * define the record sequenceID - this can be necessary to dedup events
+     */
+    public static final String kinesisSequenceID = "kx:seqID";
+    
+    /**
+     * replay flag: if the event is a replay, this flag should be set to true
+     */
+    public static final String kinesisReplayFlag = "kx:replay";
+
+}
