80424c02b52a321331994f00f29ac53cfe889105
Serge Fantino <serge@squidsolutions.com>
2014-10-20 13:34:15 +0200

    updated test case

--- com.squid.events.client/src/test/java/com/squid/events/client/TestEventModel.java
+++ com.squid.events.client/src/test/java/com/squid/events/client/TestEventModel.java
@@ -1,9 +1,13 @@
 package com.squid.events.client;
 
+import java.io.IOException;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squid.events.model.ArticleModel;
 import com.squid.events.model.EventModel;
 import com.squid.events.model.RetrievalEvent;
 
@@ -37,6 +41,34 @@ public class TestEventModel {
             String test = jackson.writeValueAsString(event);
             System.out.println(test);
         } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            ArticleModel article = new ArticleModel();
+            article.put(ArticleModel.articleContentType, "article");
+            article.put(ArticleModel.articleDiscipline, "science");
+            article.put(ArticleModel.articleJournal, "nature");
+            EventModel event = new RetrievalEvent()
+                .withContentOwnerID("myUniversity")
+                .withContentType("article")
+                .withDisplayFormat("PDF")
+                .withEntitlement("myUnivertsity")
+                .withContentReferenceArticle(article)
+                .withClientIP("127.0.0.1")
+                .withSessionID("1234")
+                .withUserID("sergio")
+                .withPageViewURL("http://something/somepage")
+                .withServerIP("127.0.0.1");
+            String test = jackson.writeValueAsString(event);
+            System.out.println(test);
+            EventModel check = jackson.readValue(test, EventModel.class);
+            Object xxx = check.get(RetrievalEvent.retrievContentReferenceArticle);
+            Assert.assertNotNull(xxx);
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
