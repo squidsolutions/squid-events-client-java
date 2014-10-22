29d0cfb4a81bf0f84f2288a49aed9ecf597602b4
Serge Fantino <serge@squidsolutions.com>
2014-10-22 17:21:27 -0400

    updated tests

--- com.squid.events.client/src/test/java/com/squid/events/client/TestEventModel.java
+++ com.squid.events.client/src/test/java/com/squid/events/client/TestEventModel.java
@@ -1,6 +1,8 @@
 package com.squid.events.client;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,10 +13,11 @@ import com.squid.events.model.ArticleModel;
 import com.squid.events.model.EventModel;
 import com.squid.events.model.RetrievalEvent;
 import com.squid.events.model.RetrievalModel;
+import com.squid.events.model.SearchEvent;
+import com.squid.events.model.StartSessionEvent;
 
 public class TestEventModel {
     
-    @Test
     public void testJson() {
         //
         ObjectMapper jackson = new ObjectMapper();
@@ -49,7 +52,6 @@ public class TestEventModel {
             ArticleModel article = new ArticleModel()
                 .withContentType("article")
                 .withDiscipline("science")
-                .withJournal("nature")
                 // more properties here...
                 ;
             EventModel event = new RetrievalEvent()
@@ -75,6 +77,73 @@ public class TestEventModel {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        // session event
+        EventModel event = new StartSessionEvent()
+        .withReferrerURL("https://google.com")
+        .withUserAgent("chrome")
+        .withAccountID("university")
+        .withAuthenticationMethod("IP")
+        .withSessionID("123")
+        .withPageViewURL("http://myapp/landingpage")
+        .withUserID("abc");
+        // search event
+        EventModel searchEvent = new SearchEvent()
+            .withEngine("quick")
+            .withFilters("format=pdf")
+            .withResultCount(100)
+            .withResultID("x1234")
+            .withResultPage(1)
+            .withTerms("some search terms")
+            .withSessionID("123")
+            .withUserID("abc");
+    }
+    
+
+    @Test
+    public void testResources() {
+        int mb = 1024*1024;
+        long mem = Runtime.getRuntime().freeMemory();
+        long start = System.currentTimeMillis();
+        List<EventModel> events = new ArrayList<EventModel>();
+        int count = 10000;
+        for (int i=0;i<count;i++) {
+            events.add(createRetrievalEvent());
+        }
+        long stop = System.currentTimeMillis();
+        long memX = Runtime.getRuntime().freeMemory();
+        System.out.println("creating "+count+" events: "+(stop-start)+"ms");
+        double latency = (stop-start)/(double)count;
+        System.out.println("event creation latency = "+latency+"ms");
+        System.out.println("memory: "+(mem-memX)/mb);
+        //
+        ObjectMapper jackson = new ObjectMapper();
+        try {
+            String test = jackson.writeValueAsString(events);
+            System.out.println("number of bytes:"+test.getBytes().length+" bytes");
+        } catch (JsonProcessingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    public EventModel createRetrievalEvent() {
+        ArticleModel article = new ArticleModel()
+            .withContentType("article")
+            .withDiscipline("science")
+            // more properties here...
+            ;
+        EventModel event = new RetrievalEvent()
+            .withContentOwnerID("myUniversity")
+            .withContentType("article")
+            .withDisplayFormat("PDF")
+            .withEntitlement("myUnivertsity")
+            .withContentReferenceArticle(article)
+            .withClientIP("127.0.0.1")
+            .withSessionID("1234")
+            .withUserID("sergio")
+            .withPageViewURL("http://something/somepage")
+            .withServerIP("127.0.0.1");
+        return event;
     }
 
 }
