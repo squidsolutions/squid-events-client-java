0484a56de535cec9a44f4aa9858515fb18e38c86
Serge Fantino <serge@squidsolutions.com>
2014-10-27 11:56:54 +0100

    add support for application version and test parameters

--- com.squid.events.client/src/main/java/com/squid/events/client/Config.java
+++ com.squid.events.client/src/main/java/com/squid/events/client/Config.java
@@ -6,6 +6,10 @@ public class Config {
     
     private String appKey = null;
     
+    private String appVersion = null;
+    
+    private boolean appTestFlag = false;
+    
     private String secretKey = null;
     
     private int maxFlusherCount = 1;
@@ -46,6 +50,22 @@ public class Config {
     public Config withAppKey(String appKey) {
         this.appKey = appKey;
         return this;
+    }
+    
+    public String getAppVersion() {
+        return appVersion;
+    }
+    
+    public void setAppVersion(String appVersion) {
+        this.appVersion = appVersion;
+    }
+    
+    public boolean isAppTestFlag() {
+        return appTestFlag;
+    }
+    
+    public void setAppTestFlag(boolean appTestFlag) {
+        this.appTestFlag = appTestFlag;
     }
     
     public String getSecretKey() {
